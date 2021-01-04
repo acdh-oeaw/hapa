2abbde69854868b4bda0e4e6125e384cb36bf46a
csae8092 <peter.andorfer@oeaw.ac.at>
2021-01-04 11:37:27 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# HAPA
+
+## History of Albanian Place-names: Albania
+
+Code repo for the FWF-Project [**Sprachgeschichte der Ortsnamen Albaniens (P 33706)**](https://pf.fwf.ac.at/de/wissenschaft-konkret/project-finder/?search[what]=P%2033706&search[science_discipline_id]=&search[promotion_category_id]=&extended=1)
---
+++ webpage/metadata.py
@@ -0,0 +1,21 @@
+# this files contains basic metadata about the project. This data will be used
+# (by default) in the base.html and index.html
+
+PROJECT_METADATA = {
+    'title': 'HAPA',
+    'author': 'Peter Andorfer',
+    'subtitle': 'History of Albanian Place-names: Albania',
+    'description': 'This project which analyzes the linguistic history of the toponymy in the Southwestern Balkans with thefocus on the place-names of Albania fits into the general research on toponymy. As with theinvestigation of toponymy in other language areas, this analysis also starts from the oldest evidenceavailable and examines the phonetic and semantic changes of place-names over time.',
+    'github': 'https://github.com/acdh-oeaw/hapa',
+    'purpose_de': 'Publikation von Forschungsdaten',
+    'purpose_en': 'Puplication of research data.',
+    'version': '0.0.1',
+    'matomo_id': 'provide some',
+    'matomo_url': '//matomo.acdh.oeaw.ac.at/',
+    'imprint': '/imprint',
+    'social_media': [
+        ('fab fa-twitter fa-2x', 'https://twitter.com/ACDH_OeAW'),
+        ('fab fa-youtube fa-2x', 'https://www.youtube.com/channel/UCgaEMaMbPkULYRI5u6gvG-w'),
+    ],
+    'app_type': 'database',  # database|website|service|tool|digital-edition
+}
