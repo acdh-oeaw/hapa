b9b154b2eee2f2604c4c4e6724096075f8995159
csae8092 <peter.andorfer@oeaw.ac.at>
2021-01-25 13:15:44 +0100

    dm first draft

--- gn_places/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GnPlacesConfig(AppConfig):
-    name = 'gn_places'
--- vocabs/apps.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8
-from django.apps import AppConfig
-
-
-class VocabsConfig(AppConfig):
-    name = 'vocabs'
