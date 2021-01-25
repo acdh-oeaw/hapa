9dbbe9832d6c33c4fa4a078bd47e4423744696d4
csae8092 <peter.andorfer@oeaw.ac.at>
2021-01-25 15:12:49 +0100

    set postgis as db default backend

--- djangobaseproject/settings/docker.py
+++ djangobaseproject/settings/docker.py
@@ -25,7 +25,7 @@ ALLOWED_HOSTS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('DB_TYP', 'django.db.backends.postgresql'),
+        'ENGINE': os.environ.get('DB_TYP', 'django.contrib.gis.db.backends.postgis'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
