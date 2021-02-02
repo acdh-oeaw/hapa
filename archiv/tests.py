b950d974deb79128c010e382d164e36d4b55e1ea
csae8092 <peter.andorfer@oeaw.ac.at>
2021-02-02 20:17:22 +0100

    simple tests added

--- archiv/tests.py
+++ archiv/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from archiv.models import HapaPlaceName
 
-# Create your tests here.
+
+class HapaPlaceNameTest(TestCase):
+    def setUp(self):
+        self.tirana = HapaPlaceName.objects.create(name="Tirana")
+
+    def test_animals_can_speak(self):
+        """Animals that can speak are correctly identified"""
+        item = HapaPlaceName.objects.get(name="Tirana")
+        self.assertEqual(item.name, 'Tirana')
