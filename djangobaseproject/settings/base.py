ce6cafb8c488f655510fddb094d18c1ccd12e76d
csae8092 <peter.andorfer@oeaw.ac.at>
2021-03-18 18:43:27 +0100

    removed STATICFILES_DIRS

--- djangobaseproject/settings/base.py
+++ djangobaseproject/settings/base.py
@@ -129,9 +129,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 ARCHE_SETTINGS = {
     'project_name': "hapa",
