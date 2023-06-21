1dcc6d6f863d480315a2376f0be9b65dfb488c97
csae8092 <peter.andorfer@oeaw.ac.at>
2023-06-21 13:38:07 +0200

    removed modularized settings

--- djangobaseproject/urls.py
+++ djangobaseproject/urls.py
@@ -5,29 +5,23 @@ from vocabs import api_views
 
 
 router = routers.DefaultRouter()
-router.register(r'skosconceptschemes', api_views.SkosConceptSchemeViewSet)
-router.register(r'skoscollections', api_views.SkosCollectionViewSet)
-router.register(r'skosconcepts', api_views.SkosConceptViewSet)
+router.register(r"skosconceptschemes", api_views.SkosConceptSchemeViewSet)
+router.register(r"skoscollections", api_views.SkosCollectionViewSet)
+router.register(r"skosconcepts", api_views.SkosConceptViewSet)
 
 urlpatterns = [
-    url(r'^api/', include(router.urls)),
-    url(
-        r'^api-auth/',
-        include('rest_framework.urls', namespace='rest_framework')
-    ),
-    url(r'^admin/', admin.site.urls),
-    url(r'^browsing/', include('browsing.urls', namespace='browsing')),
-    url(r'^info/', include('infos.urls', namespace='info')),
-    url(r'^archiv/', include('archiv.urls', namespace='archiv')),
-    url(r'^archiv-ac/', include('archiv.dal_urls', namespace='archiv-ac')),
-    url(r'^bib/', include('bib.urls', namespace='bib')),
-    url(
-        r'^gn-places-ac/',
-        include('gn_places.dal_urls', namespace='gn_places-ac')
-    ),
-    url(r'^vocabs/', include('vocabs.urls', namespace='vocabs')),
-    url(r'^vocabs-ac/', include('vocabs.dal_urls', namespace='vocabs-ac')),
-    url(r'^', include('webpage.urls', namespace='webpage')),
+    url(r"^api/", include(router.urls)),
+    url(r"^api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    url(r"^admin/", admin.site.urls),
+    url(r"^browsing/", include("browsing.urls", namespace="browsing")),
+    url(r"^info/", include("infos.urls", namespace="info")),
+    url(r"^archiv/", include("archiv.urls", namespace="archiv")),
+    url(r"^archiv-ac/", include("archiv.dal_urls", namespace="archiv-ac")),
+    url(r"^bib/", include("bib.urls", namespace="bib")),
+    url(r"^gn-places-ac/", include("gn_places.dal_urls", namespace="gn_places-ac")),
+    url(r"^vocabs/", include("vocabs.urls", namespace="vocabs")),
+    url(r"^vocabs-ac/", include("vocabs.dal_urls", namespace="vocabs-ac")),
+    url(r"^", include("webpage.urls", namespace="webpage")),
 ]
 
-handler404 = 'webpage.views.handler404'
+handler404 = "webpage.views.handler404"
