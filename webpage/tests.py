70b8daa3820cde0f8219b2948c58ff2a3c0e051c
csae8092 <peter.andorfer@oeaw.ac.at>
2024-07-16 09:08:52 +0200

    test for imprint view

--- webpage/tests.py
+++ webpage/tests.py
@@ -1,13 +1,15 @@
 from django.contrib.auth.models import User
 from django.test import Client, TestCase
 
+from django.urls import reverse
+
 
 class WebpageTest(TestCase):
     def setUp(self):
         self.client = Client()
         User.objects.create_user("temporary", "temp@gmail.com", "temporary")
 
-    def test_webpage(self):
+    def test_001_webpage(self):
         rv = self.client.get("/")
         self.assertEqual(rv.status_code, 302)
         rv = self.client.get("/accounts/login/")
@@ -20,3 +22,8 @@ class WebpageTest(TestCase):
         form_data = {"username": "non_exist", "password": "temporary"}
         rv = self.client.post("/accounts/login/", form_data, follow=True)
         self.assertContains(rv, "user does not exist")
+
+    def test_002_imprint(self):
+        url = reverse("webpage:imprint")
+        rv = self.client.get(url)
+        self.assertContains(rv, "Legal disclosure")
